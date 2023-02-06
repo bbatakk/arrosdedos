a99d589a34de9c1ba961aa1a62d72d7d01a5c2d6
Alex Abad <alexabad08@gmail.com>
2023-02-06 13:29:06 +0100

    contruccio de taula, pagina create i pagina index

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,60 @@
+@extends('products.layout')
+
+@section('content')
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2>Add New Product</h2>
+        </div>
+        <div class="pull-right">
+            <a class="btn btn-primary" href="{{ route('products.index') }}"> Back</a>
+        </div>
+    </div>
+</div>
+
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <strong>Whoops!</strong> There were some problems with your input.<br><br>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
+<form action="{{ route('products.store') }}" method="POST">
+    @csrf
+
+     <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Nom:</strong>
+                <input type="text" name="nom" class="form-control" placeholder="Nom">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Cognom:</strong>
+                <input type="text" name="cognom" class="form-control" placeholder="Cognom">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Telefon:</strong>
+                <input type="text" name="telefon" class="form-control" placeholder="Telefon">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Arros:</strong>
+                <textarea class="form-control" style="height:150px" name="arros" placeholder="Arros"></textarea>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </div>
+
+</form>
+@endsection
---
+++ resources/views/products/layout.blade.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Arros de dos</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    </head>
+    <body>
+        <h1>Arros de dos</h1>
+        <div class="container">
+            <br>
+            @yield('content')
+        </div>
+    </body>
+</html>
